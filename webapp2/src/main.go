02d12d61498f726dea2dfda3121f6f0f79518d3e
unagi3107 <urano0717@gmail.com>
2022-06-25 23:20:48 +0900

    net/http using HTTP2

--- webapp2/src/main.go
+++ webapp2/src/main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"golang.org/x/net/http2"
 )
 
+type MyHandler struct{}
+
+func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
+
 func main() {
+	handler := MyHandler{}
 	server := http.Server{
-		Addr:    "127.0.0.1:8443",
-		Handler: nil,
+		Addr:    "127.0.0.1:8080",
+		Handler: &handler,
 	}
-	err := server.ListenAndServeTLS("../cert/cert.pem", "../cert/key.pem")
+	http2.ConfigureServer(&server, &http2.Server{})
+	err := server.ListenAndServeTLS("webapp2/cert/cert.pem", "webapp2/cert/key.pem")
 	if err != nil {
 		log.Fatal(err)
 	}
